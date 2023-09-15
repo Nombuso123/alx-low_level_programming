5211b0f97a01d5d8ed1f95851a0da047eb724c9a
Nombuso Dlamini <nombusopuritydlamini123@gmail.com>
2023-09-15 04:29:57 -0700

    0x03 debbuging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - cause an indifinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("indifinite loop incoming :(\n");
+
+	i = 0;
+
+	/* while(i < 10)*/
+	/*{*/
+	/* putchar(i):*/
+	/*}*/
+
+	printf("indifinite loop avoided \\o/\n");
+
+	return (0);
+}
