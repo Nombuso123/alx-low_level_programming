5f94970cd459d8dcc6d93a458243d88ec7988504
Nombuso Dlamini <nombusopuritydlamini123@gmail.com>
2023-09-19 13:35:37 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 	int longi = 0;
 	int o;
 
-	while (*s != '\n')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string 
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	int a, n, long;
+
+	longi = 0;
+	
+	for (a = 0; str[a] =! '\0'; a++)
+}
