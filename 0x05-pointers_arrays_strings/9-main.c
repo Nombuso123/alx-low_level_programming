8e57cdb54b6ce536479c4ec1a367584659cc4692
Nombuso Dlamini <nombusopuritydlamini123@gmail.com>
2023-09-25 15:21:18 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -1,22 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-char *_strcpy(char *dest, char *src)
+int main(void)
 {
-int l = 0;
-int x = 0;
-while (*(src + l) != '\0')
-{
-l++;
-}
-for ( ; x < l ; x++)
-{
-dest[x] = src[x];
-}
-dest[l] = '\0';
-return (dest);
+    char s1[98];
+    char *ptr;
+
+    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+    printf("%s", s1);
+    printf("%s", ptr);
+    return (0);
 }
